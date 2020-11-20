b941f9e411996d74c402770ea0f8a218640bdd41
Bobby <bobbysebolao@gmail.com>
2020-11-20 18:16:17 +0000

    Add size, clear, getFirst & getLast helper methods

--- linked_list.js
+++ linked_list.js
@@ -8,6 +8,24 @@ class ListNode {
 class LinkedList {
     constructor(head = null) {
         this.head = head;
+        this.size = () => {
+            let count = 0;
+            let currNode = this.head
+            while (currNode) {
+                currNode = currNode.next;
+                count++;
+            }
+            return count;
+        }
+        this.clear = () => this.head = null;
+        this.getFirst = () => this.head;
+        this.getLast = () => {
+            let lastNode = this.head
+            while (lastNode.next) {
+                lastNode = lastNode.next;
+            }
+            return lastNode;
+        }
     }
 }
 
@@ -22,7 +40,7 @@ const toLinkedList = (arr) => {
         else listNodes[i].next = null;
     }
     const linkedList =  new LinkedList(listNodes[0]);
-    console.log(linkedList);
+    console.log(linkedList.clear());
     return linkedList;
 }
 
